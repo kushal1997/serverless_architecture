bec86f5788016877ea86f759ae9fab46ebb83199
kushal1997 <kushalrao103@gmail.com>
2025-06-18 00:22:11 +0530

    WIP: Code is good, readme is pending

--- Assignment-2/lambda_function.py
+++ Assignment-2/lambda_function.py
@@ -2,12 +2,12 @@ import boto3
 import datetime
 
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 def delete_old_files():
     bucket_name = os.environ["BUCKET_NAME"]
-    days_to_check = 0
+    days_to_check = 30
 
     s3 = boto3.client('s3')
     response = s3.list_objects_v2(Bucket=bucket_name)
@@ -23,7 +23,7 @@ def delete_old_files():
 
         print(f'difference in days for {content['Key']} is {days_difference}')
 
-        if days_difference == days_to_check:
+        if days_difference > days_to_check:
             print(f"deleting old files {content["Key"]} having days difference of {days_difference} days")
             s3.delete_object(Bucket=bucket_name, Key=content['Key'])
             deleted_old_files.append(content['Key'])
@@ -35,6 +35,7 @@ def delete_old_files():
     else:
         print("There are no older files to delete")
 
-delete_old_files()
+def lambda_handler(event, context):
+    delete_old_files()
 
 
